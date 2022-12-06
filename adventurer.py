ccce31155c20909a5fd9bcad0a487d47b84cb580
Mel <mel@mel.codes>
2022-12-05 19:38:39 -0800

    adds update_health() to change the HP of the player

--- adventurer.py
+++ adventurer.py
@@ -2,8 +2,10 @@ import random
 
 
 class Adventurer:
+    """This class creates an Adventurer that will traverse the dungeon."""
 
     def __init__(self, name):
+        # randomly generate the adventurer's health
         health = random.randint(75, 100)
 
         self._name = name
@@ -22,7 +24,15 @@ class Adventurer:
             "polymorphism": 0,
         }
 
+    def update_health(self, strength, is_damage=False):
+        """This method updates the current HP of the Adventurer."""
+        if is_damage:
+            self._health['current'] -= strength
+        else:
+            self._health['current'] += strength
+
     def __str__(self):
+        """This method returns a string representation of the Adventurer."""
         return self._name
 
 
