d91833e4d28e176bd3339d846025c577d6aba86f
Mel <mel@mel.codes>
2022-12-05 19:32:32 -0800

    adds init method

--- adventurer.py
+++ adventurer.py
@@ -1,2 +1,31 @@
+import random
+
+
 class Adventurer:
-    pass
+
+    def __init__(self, name):
+        health = random.randint(75, 100)
+
+        self._name = name
+        self._health = {
+            "current": health,
+            "max": health
+        }
+        self._potions = {
+            "healing": 2,
+            "vision": 2,
+        }
+        self._pillars = {
+            "abstraction": 0,
+            "encapsulation": 0,
+            "inheritance": 0,
+            "polymorphism": 0,
+        }
+
+    def __str__(self):
+        return self._name
+
+
+# test
+player = Adventurer("Mel")
+print(player)
