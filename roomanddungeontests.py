6dcd26ffbc6c9470862de97b43a7e9cb1e535514
sheehan1025 <118309783+sheehan1025@users.noreply.github.com>
2022-12-12 14:56:44 -0800

    Update roomanddungeontests.py
    Additional unit tests on class methods

--- roomanddungeontests.py
+++ roomanddungeontests.py
@@ -12,10 +12,28 @@ class DungeonAndRoomTests(unittest.TestCase):
 
     def testCreateRoom(self):
         self.assertIsInstance(self.room, Room, "A Room was not created")
+    
+    def testIsExit(self):
+        self.room.exit = True
+        self.assertTrue(self.room.is_exit(), "An exit room was not returned")
+        
+    def testHasPit(self):
+        self.room.pit = True
+        self.assertTrue(self.room.has_pit(), "A room with a pit was not returned")
+    
+    def testHasPotion(self):
+        self.room.healing_potion = True
+        self.assertTrue(self.room.has_potion("healing"), "A room with a healing potion was not returned")
+    
+    def testHasPillar(self):
+        self.room.abstraction = True
+        self.assertTrue(self.room.has_pillar("abstraction"), "The room didn't return the abstraction pillar")
 
     def testCreateDungeon(self):
         self.assertIsInstance(self.dungeon, Dungeon, "A Dungeon was not created")
+        
+    def testGetRoom(self):
+        self.assertIs(self.dungeon.get_room(0,0), self.dungeon.rooms[0][0], "Failed to get the correct room")
 
 
 unittest.main()
-
